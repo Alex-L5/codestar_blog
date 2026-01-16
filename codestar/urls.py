7b1b36e6410f2063f16dabb85bdd691ddf84f887
Alex-L5 <almenkiti@aol.com>
2026-01-16 11:07:04 +0000

    Secure SECRET_KEY with environment variable

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from django.urls import path, include
+from django.urls import path, include
 from blog import views as blog_views
 
 urlpatterns = [
-    # path("", include("blog.urls"), name="blog-urls"),
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
     path('blog/', blog_views.my_blog, name="blog"),  # the app urls are loaded as the main urls
-    # path('summernote/', include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
     ]
